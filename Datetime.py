dff342f7e21189abf0fb415088c96ec34cb3c54c
timevgen713 <57857781+timevgen713@users.noreply.github.com>
2019-11-27 16:24:47 +0500

    Update Datetime.py

--- Datetime.py
+++ Datetime.py
@@ -10,15 +10,23 @@ def msg_last_time_format():
     return strftime('%y.%m.%d', gmtime())
 
 def time_now():
-    # время по мск
-    return datetime.datetime.now(tz=msk).strftime('%H:%M:%S MSK (UTC +3)')
-
-
+	# время по мск
+	return datetime.datetime.now(tz=msk).strftime('%H:%M:%S')
+def time_nowdate():
+	# time_now() без перевода результата в строку
+	return datetime.datetime.now(tz=msk)
 def datetime_now():
-    # время и дата по мск
-    return datetime.datetime.now(tz=msk).strftime('%Y-%m-%d %H:%M:%S  MSK (UTC +3)')
-
-
+	# время и дата по мск
+	 return datetime.datetime.now(tz=msk).strftime('%Y-%m-%d %H:%M:%S')
 def date_now():
-    # дата по мск
-    return datetime.datetime.now(tz=msk).strftime('%Y-%m-%d')
+	# дата по мск
+	return datetime.datetime.now(tz=msk).strftime('%Y-%m-%d')
+def time_add(delta):
+	# сложение дат
+	now = time_nowdate()
+	add = delta.split('.')
+	add = list(map(int, add))
+	d = datetime.timedelta(hours=add[0], minutes=add[1],seconds=add[2])
+	res = now + d
+	res = str(res)
+	return res[:19:]
