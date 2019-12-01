49138a5506fc41dcaca6c28d435b13572bd0145a
timevgen713 <57857781+timevgen713@users.noreply.github.com>
2019-12-01 17:32:50 +0300

    Update Datetime.py

--- Datetime.py
+++ Datetime.py
@@ -7,22 +7,25 @@ msk = pytz.timezone('Europe/Moscow')
 
 def time_now():
 	# время по мск
-	return datetime.datetime.now(tz=msk).strftime('%H:%M:%S')
-def time_nowdate():
-	# time_now() без перевода результата в строку
-	return datetime.datetime.now(tz=msk)
-def datetime_now():
-	# время и дата по мск
-	 return datetime.datetime.now(tz=msk).strftime('%Y-%m-%d %H:%M:%S')
+	return datetime.datetime.now(tz=msk).strftime('%H:%M:%S (UTC +3 MSK)')
+
 def date_now():
 	# дата по мск
 	return datetime.datetime.now(tz=msk).strftime('%Y-%m-%d')
+
+def datetime_now():
+	# время и дата по мск
+	 return datetime.datetime.now(tz=msk).strftime('%Y-%m-%d %H:%M:%S (UTC +3 MSK)')
+
+def time_nowdate():
+	# time_now() без перевода результата в строку
+	return datetime.datetime.now(tz=msk)
 def time_add(delta):
 	# сложение дат
 	now = time_nowdate()
 	add = delta.split('.')
 	add = list(map(int, add))
-	d = datetime.timedelta(hours=add[0], minutes=add[1],seconds=add[2])
+	d = datetime.timedelta(hours=add[0], minutes=add[1], seconds=add[2])
 	res = now + d
 	res = str(res)
 	return res[:19:]
