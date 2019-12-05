6f6838e36d19cfbb0031a4016ef0dcbab8962511
timevgen713 <57857781+timevgen713@users.noreply.github.com>
2019-12-05 20:23:07 +0300

    Update User.py

--- User.py
+++ User.py
@@ -32,96 +32,94 @@ def clearList(dict_of_users):
 
 # Remove user and check data validation( not null, it;s integer and user in chat) else send not found.
 def remove_user(event, session_api, chat_id, msg_command, dict_of_users):
-    # if Connector.db_isAdmin(chat_id, event.object.message['from_id']):
-    getConversationMembers(session_api, event, dict_of_users)
-    # check forward messages
-    if len(event.object.message['fwd_messages']) != 0:
-        for temp in event.object.message['fwd_messages']: \
-                Messages.remove_user(chat_id, int(temp['from_id']))
+    if Connector.db_isAdmin(chat_id, event.object.message['from_id']):
+        getConversationMembers(session_api, event, dict_of_users)
+        # check forward messages
+        if len(event.object.message['fwd_messages']) != 0:
+            for temp in event.object.message['fwd_messages']: \
+                    Messages.remove_user(chat_id, int(temp['from_id']))
 
-    # check id in msg_command
-    try:
-        msg_ids = msg_command.split()[1:]
-        for user_idd in msg_ids:
-            user_id = (str(user_idd[3:]).split('|'))[0]
-            if int(user_id) in dict_of_users.keys():
-                Messages.remove_user(chat_id, user_id)
-            else:
-                Messages.send_message(session_api, chat_id,
-                                      'User with id: \'' + str(user_id) + '\' is not in chat!')
-    except ValueError:
-        Messages.send_message(session_api, chat_id, 'Id is incorrect!')
-    # else:
-    #     Messages.send_message(session_api, chat_id, 'Permission denied! You is not admin!')
+        # check id in msg_command
+        try:
+            msg_ids = msg_command.split()[1:]
+            for user_idd in msg_ids:
+                user_id = (str(user_idd[3:]).split('|'))[0]
+                if int(user_id) in dict_of_users.keys():
+                    Messages.remove_user(chat_id, user_id)
+                else:
+                    Messages.send_message(session_api, chat_id,
+                                          'User with id: \'' + str(user_id) + '\' is not in chat!')
+        except ValueError:
+            Messages.send_message(session_api, chat_id, 'Id is incorrect!')
+    else:
+        Messages.send_message(session_api, chat_id, 'Permission denied! You is not admin!')
 
 
 def warn_user(event, session_api, chat_id, msg_command, dict_of_users):
-    # if Connector.db_isAdmin(chat_id, event.object.message['from_id']):
-    getConversationMembers(session_api, event, dict_of_users)
-    # check forward messages
-    if len(event.object.message['fwd_messages']) != 0:
-        for temp in event.object.message['fwd_messages']:
-            Messages.send_message(session_api, chat_id,
-                                  'User with id: \'' + str(int(temp['from_id'])) + '\' warned successful!')
-            # Connector.db_warn(chat_id, int(temp['from_id']))
-
-    # check id in msg_command
-    try:
-        msg_ids = msg_command.split()[1:]
-        for user_idd in msg_ids:
-            user_id = (str(user_idd[3:]).split('|'))[0]
-            if int(user_id) in dict_of_users.keys():
+    if Connector.db_isAdmin(chat_id, event.object.message['from_id']):
+        getConversationMembers(session_api, event, dict_of_users)
+        # check forward messages
+        if len(event.object.message['fwd_messages']) != 0:
+            for temp in event.object.message['fwd_messages']:
                 Messages.send_message(session_api, chat_id,
-                                      'User with id: \'' + str(user_id) + '\' warned successful!')
+                                      'User with id: \'' + str(int(temp['from_id'])) + '\' warned successful!')
                 # Connector.db_warn(chat_id, int(temp['from_id']))
-            else:
-                Messages.send_message(session_api, chat_id,
-                                      'User with id: \'' + str(user_id) + '\' is not in chat!')
-    except ValueError:
-        Messages.send_message(session_api, chat_id, 'Id is incorrect!')
-    # else:
-    #     Messages.send_message(session_api, chat_id, 'Permission denied! You is not admin!')
+
+        # check id in msg_command
+        try:
+            msg_ids = msg_command.split()[1:]
+            for user_idd in msg_ids:
+                user_id = (str(user_idd[3:]).split('|'))[0]
+                if int(user_id) in dict_of_users.keys():
+                    Messages.send_message(session_api, chat_id,
+                                          'User with id: \'' + str(user_id) + '\' warned successful!')
+                    # Connector.db_warn(chat_id, int(temp['from_id']))
+                else:
+                    Messages.send_message(session_api, chat_id,
+                                          'User with id: \'' + str(user_id) + '\' is not in chat!')
+        except ValueError:
+            Messages.send_message(session_api, chat_id, 'Id is incorrect!')
+    else:
+        Messages.send_message(session_api, chat_id, 'Permission denied! You is not admin!')
 
 
 def unwarn_user(event, session_api, chat_id, msg_command, dict_of_users):
-    # if Connector.db_isAdmin(chat_id, event.object.message['from_id']):
-    getConversationMembers(session_api, event, dict_of_users)
-    # check forward messages
-    if len(event.object.message['fwd_messages']) != 0:
-        for temp in event.object.message['fwd_messages']:
-            Messages.send_message(session_api, chat_id,
-                                  'User with id: \'' + str(int(temp['from_id'])) + '\' unwarned successful!')
-            # Connector.db_unwarn(chat_id, int(temp['from_id']))
-
-    # check id in msg_command
-    try:
-        msg_ids = msg_command.split()[1:]
-        for user_idd in msg_ids:
-            user_id = (str(user_idd[3:]).split('|'))[0]
-            if int(user_id) in dict_of_users.keys():
+    if Connector.db_isAdmin(chat_id, event.object.message['from_id']):
+        getConversationMembers(session_api, event, dict_of_users)
+        # check forward messages
+        if len(event.object.message['fwd_messages']) != 0:
+            for temp in event.object.message['fwd_messages']:
                 Messages.send_message(session_api, chat_id,
-                                      'User with id: \'' + str(user_id) + '\' unwarned successful!')
+                                      'User with id: \'' + str(int(temp['from_id'])) + '\' unwarned successful!')
                 # Connector.db_unwarn(chat_id, int(temp['from_id']))
-            else:
-                Messages.send_message(session_api, chat_id,
-                                      'User with id: \'' + str(user_id) + '\' is not in chat!')
-    except ValueError:
-        Messages.send_message(session_api, chat_id, 'Id is incorrect!')
+
+        # check id in msg_command
+        try:
+            msg_ids = msg_command.split()[1:]
+            for user_idd in msg_ids:
+                user_id = (str(user_idd[3:]).split('|'))[0]
+                if int(user_id) in dict_of_users.keys():
+                    Messages.send_message(session_api, chat_id,
+                                          'User with id: \'' + str(user_id) + '\' unwarned successful!')
+                    # Connector.db_unwarn(chat_id, int(temp['from_id']))
+                else:
+                    Messages.send_message(session_api, chat_id,
+                                          'User with id: \'' + str(user_id) + '\' is not in chat!')
+        except ValueError:
+            Messages.send_message(session_api, chat_id, 'Id is incorrect!')
+    else:
+        Messages.send_message(session_api, chat_id, 'Permission denied! You is not admin!')
 
 
-# else:
-#     Messages.send_message(session_api, chat_id, 'Permission denied! You is not admin!')
-
-
-def kickfrom_user(session_api, chat_id, msg_command):
-    # if Connector.db_isAdmin(chat_id, event.object.message['from_id']):
-    try:
-        time = msg_command.split(' ')[1]
-        # Connector.db_kickfrom(chat_id, time)
-    except ValueError:
-        Messages.send_message(session_api, chat_id, 'Time is incorrect!')
-    # else:
-    #     Messages.send_message(session_api, chat_id, 'Permission denied! You is not admin!')
+def kickfrom_user(event, session_api, chat_id, msg_command):
+    if Connector.db_isAdmin(chat_id, event.object.message['from_id']):
+        try:
+            time = msg_command.split(' ')[1]
+            # Connector.db_kickfrom(chat_id, time)
+        except ValueError:
+            Messages.send_message(session_api, chat_id, 'Time is incorrect!')
+    else:
+        Messages.send_message(session_api, chat_id, 'Permission denied! You is not admin!')
 
 
 def get_state(event, session_api, chat_id, msg_command, dict_of_users):
