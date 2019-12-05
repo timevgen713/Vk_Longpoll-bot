ca7f88150dd4920a4ffef1249926918db36f2543
timevgen713 <57857781+timevgen713@users.noreply.github.com>
2019-12-05 21:08:14 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,112 +1,113 @@
 import pymysql
-import Datetime as date
+import date
 import datetime
+###############################    chat_id и user_id  - строки #####################################
 host = 'localhost'
 user = 'root'
 psw = ''
 bd_name = 'vkbot'
-def bd_ban(id,time):
-	res = date.time_add(time)
+'''def new_chat(chat_id,admin_id,members):
 	con = pymysql.connect(host, user,psw, bd_name)
 	with con:	    
 	    cur = con.cursor()
-	    sql = "UPDATE `chatip` SET `ban`='"+res+"' WHERE `id`='"+id+"'"
+	    sql = "CREATE TABLE `"+chat_id+"`(`id` text NOT NULL,`adminlvl` int(11) NOT NULL DEFAULT '0',`warns` int(11) NOT NULL DEFAULT '0',`mute` text NOT NULL,`ban` text NOT NULL,`msgcount` int(11) NOT NULL DEFAULT '0')"
 	    cur.execute(sql)
-def bd_unban(id):
+	    for i in range(len(members)):
+	    	if members[i]==admin_id:
+	    		cur.execute("INSERT INTO `"+chat_id+"`(`id`) VALUES ('"+members[i]+"');")
+	    		cur.execute("UPDATE`"+chat_id+"` SET `adminlvl`='1' WHERE `id`='"+admin_id+"';")
+	    	else:
+	    		cur.execute("INSERT INTO `"+chat_id+"`(`id`) VALUES ('"+members[i]+"');")'''
+def db_promote(chat_id, user_id):
 	con = pymysql.connect(host, user,psw, bd_name)
 	with con:	    
 	    cur = con.cursor()
-	    sql = "UPDATE `chatip` SET `ban`='' WHERE `id`='"+id+"'"
+	    sql = "UPDATE `"+chat_id+"` SET `adminlvl`='1' WHERE `id`='"+user_id+"';"
 	    cur.execute(sql)
-def bd_bancheck():
+def db_drop(chat_id, user_id):
 	con = pymysql.connect(host, user,psw, bd_name)
 	with con:	    
 	    cur = con.cursor()
-	    sql = "SELECT * FROM `chatip` WHERE `ban` is NOT NULL"
-	    cur.execute(sql)
-	    rows = cur.fetchall()
-	    now = datetime.datetime.strptime(date.datetime_now(),'%Y-%m-%d %H:%M:%S')
-	    for row in rows:
-	    	try:
-	    		unbandate = datetime.datetime.strptime(row[4],'%Y-%m-%d %H:%M:%S')
-	    		if now>=unbandate:
-	    			bd_unban(row[0])
-	    	except ValueError:
-	    		pass
-def bd_mute(id,time):
-	res = date.time_add(time)
-	con = pymysql.connect(host, user,psw, bd_name)
-	with con:	    
-	    cur = con.cursor()
-	    sql = "UPDATE `chatip` SET `mute`='"+res+"' WHERE `id`='"+id+"'"
-	    cur.execute(sql)
-def bd_unmute(id):
-	con = pymysql.connect(host, user,psw, bd_name)
-	with con:	    
-	    cur = con.cursor()
-	    sql = "UPDATE `chatip` SET `mute`='' WHERE `id`='"+id+"'"
-	    cur.execute(sql)
-def bd_mutecheck():
-	con = pymysql.connect(host, user,psw, bd_name)
-	with con:	    
-	    cur = con.cursor()
-	    sql = "SELECT * FROM `chatip` WHERE `mute` is NOT NULL"
-	    cur.execute(sql)
-	    rows = cur.fetchall()
-	    now = datetime.datetime.strptime(date.datetime_now(),'%Y-%m-%d %H:%M:%S')
-	    for row in rows:
-	    	try:
-	    		unmutedate = datetime.datetime.strptime(row[3],'%Y-%m-%d %H:%M:%S')
-	    		if now>unmutedate:
-	    			bd_unmute(row[0])
-	    	except ValueError:
-	    		pass
-def bd_promote(id):
-	con = pymysql.connect(host, user,psw, bd_name)
-	with con:	    
-	    cur = con.cursor()
-	    sql = "UPDATE `chatip` SET `adminlvl`='1' WHERE `id`='"+id+"'"
-	    cur.execute(sql)
-def bd_drop(id):
-	con = pymysql.connect(host, user,psw, bd_name)
-	with con:	    
-	    cur = con.cursor()
-	    sql = "UPDATE `chatip` SET `adminlvl`='0' WHERE `id`='"+id+"'"
+	    sql = "UPDATE `"+chat_id+"` SET `adminlvl`='0' WHERE `id`='"+user_id+"';"
 	    cur.execute(sql)
 
-def bd_msgcountadd(id):
+def db_msgcountadd(chat_id,user_id):
 	con = pymysql.connect(host, user,psw, bd_name)
 	with con:	    
 	    cur = con.cursor()
-	    sql = "SELECT * FROM `chatip` WHERE `id`='"+id+"'"
+	    sql = "SELECT * FROM `"+chat_id+"` WHERE `id`='"+user_id+"';"
 	    cur.execute(sql)
 	    rows = cur.fetchone()
-	    msgcount = int(rows[5])+1
-	    sql = "UPDATE `chatip` SET `msgcount`='"+str(msgcount)+"' WHERE `id`='"+id+"'"
+	    msgcount = int(rows[3])+1
+	    sql = "UPDATE `"+chat_id+"` SET `msgcount`='"+str(msgcount)+"' WHERE `id`='"+user_id+"';"
 	    cur.execute(sql)
-
-def bd_warn(id):
+def db_get_msg_amount(chat_id, user_id):
 	con = pymysql.connect(host, user,psw, bd_name)
 	with con:	    
 	    cur = con.cursor()
-	    sql = "SELECT * FROM `chatip` WHERE `id`='"+id+"'"
+	    sql = "SELECT * FROM `"+chat_id+"` WHERE `id`='"+user_id+"';"
+	    cur.execute(sql)
+	    rows = cur.fetchone()
+	    msgcount = int(rows[3])
+	    return msgcount
+def db_warn(chat_id,user_id):
+	con = pymysql.connect(host, user,psw, bd_name)
+	with con:	    
+	    cur = con.cursor()
+	    sql = "SELECT * FROM `"+chat_id+"` WHERE `id`='"+user_id+"';"
 	    cur.execute(sql)
 	    rows = cur.fetchone()
 	    warns = int(rows[2])+1
-	    if warns>2:
-	    	warns = 0
-	    	bd_ban(id,'168.0.0')
-	    sql = "UPDATE `chatip` SET `warns`='"+str(warns)+"' WHERE `id`='"+id+"'"
-	    cur.execute(sql)
-def bd_unwarn(id):
+	    if warns==3:
+	    	# функция кика с беседы
+	    	pass
+def db_unwarn(chat_id, user_id):
 	con = pymysql.connect(host, user,psw, bd_name)
 	with con:	    
 	    cur = con.cursor()
-	    sql = "SELECT * FROM `chatip` WHERE `id`='"+id+"'"
+	    sql = "SELECT * FROM `"+chat_id+"` WHERE `id`='"+user_id+"';"
 	    cur.execute(sql)
 	    rows = cur.fetchone()
 	    warns = int(rows[2])
 	    if warns!=0:
 	    	warns -=1
-	    sql = "UPDATE `chatip` SET `warns`='"+str(warns)+"' WHERE `id`='"+id+"'"
+	    sql = "UPDATE `"+chat_id+"` SET `warns`='"+str(warns)+"' WHERE `id`='"+user_id+"';"
 	    cur.execute(sql)
+def db_getWarns(chat_id, user_id):
+	con = pymysql.connect(host, user,psw, bd_name)
+	with con:	    
+	    cur = con.cursor()
+	    sql = "SELECT * FROM `"+chat_id+"` WHERE `id`='"+user_id+"';"
+	    cur.execute(sql)
+	    rows = cur.fetchone()
+	    warns = int(rows[2])
+	    return warns
+def isAdmin(chat_id,user_id):
+	con = pymysql.connect(host,user,psw,bd_name)
+	with con:
+		cur = con.cursor()
+		sql = "SELECT * FROM `"+chat_id+"` WHERE `id`='"+user_id+"';"
+		cur.execute(sql)
+		rows = cur.fetchone()
+		if rows==None:
+			return False
+		else:
+			if rows[1]==1:
+				return True
+			else:
+				return False
+def db_msg_last_time_add(chat_id, user_id):
+	con = pymysql.connect(host, user,psw, bd_name)
+	with con:	    
+	    cur = con.cursor()
+	    sql = "UPDATE `"+chat_id+"` SET `last_message`='"+date.datetime_now()+"' WHERE `id`='"+user_id+"';"
+	    cur.execute(sql)
+def db_get_msg_last_time(chat_id, user_id):
+	con = pymysql.connect(host,user,psw,bd_name)
+	with con:
+		cur = con.cursor()
+		sql = "SELECT * FROM `"+chat_id+"` WHERE `id`='"+user_id+"';"
+		cur.execute(sql)
+		rows = cur.fetchone()
+		return rows[4]
+	
